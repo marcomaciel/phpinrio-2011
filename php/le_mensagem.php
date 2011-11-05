6eace71e829f08572c507d7d32c57f825b075a3c
Marco Antonio de Azevedo Maciel <maciel3000@gmail.com>
2011-11-05 12:36:52 -0200

    comentario no codigo

--- php/le_mensagem.php
+++ php/le_mensagem.php
@@ -1,7 +1,20 @@
-<?php 
-	//$mensagem = file_get_contents('http://localhost/phpinrio/obtem_mensagem.php');
-	
+<?php 	
 	//Obtem o valor informado pelo arquivo referenciado.	
 	$mensagem = file_get_contents('http://mmaciel.com.br/phpinrio/obtem_mensagem.php');	
-	print $mensagem;
+	
+	//Abre comunicação com a porta serial.
+	$portaserial = fopen('/dev/tty.usbserial-A600bWE7', 'w');
+	
+	//Em alguns casos a Arduino pode reiniciar, por isso
+	//é bom esperar para enviar informação depois de conectar
+	sleep(3);
+	
+	//Envia mensagem para a porta seral.
+	fwrite($portaserial, $mensagem);
+	
+	//Espera para que o dado enviado pelo PHP chegue até a Arduino
+	sleep(1);
+	
+	//Fecha comunicação com a porta serial.
+	//fclose($portaserial, mensagem);
 ?>
